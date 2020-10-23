f70fa419a163aad5c1f52a1b1fdccb3d50ee1d42
w270 <wangzhibinjx@qq.com>
2020-10-23 23:49:38 +0800

    springmvc maven

---
+++ hello-springmvc-maven/src/main/java/com/zbinyun/demo/web/DemoController.java
@@ -0,0 +1,12 @@
+package com.zbinyun.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DemoController {
+    @RequestMapping("/index")
+    public String hello(){
+        return "index";
+    }
+}
